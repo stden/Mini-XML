3ce617b03b3cd2ad2d6fb11f652f6c6db3571eae
Michael R Sweet <michaelrsweet@gmail.com>
2012-01-01 00:21:57 -0800

    Add repository link and update copyrights.

--- common.js
+++ common.js
@@ -8,6 +8,7 @@ XML<br>\
   <li><a href="documentation/index.html">Documentation</a></li>\
   <li><a href="https://github.com/michaelrsweet/Mini-XML/downloads">Download</a></li>\
   <li><a href="https://github.com/michaelrsweet/Mini-XML/issues">Issues</a></li>\
+  <li><a href="https://github.com/michaelrsweet/Mini-XML">Repository</a></li>\
   <li><a href="https://github.com/michaelrsweet/Mini-XML/wiki">Wiki</a></li>\
 </ul>';
 
