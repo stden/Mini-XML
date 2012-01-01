d15fcf390e5019ce0ea48a680d76a03fb5f9b63e
Michael R Sweet <michaelrsweet@gmail.com>
2011-12-31 23:34:00 -0800

    Fill in actual site pages.

---
+++ common.js
@@ -0,0 +1,24 @@
+/* Sidebar loading code - we do it here to avoid changing it on every page */
+function load_sidebar() {
+  contents =
+'<p>mini<br>\
+XML<br>\
+<ul>\
+  <li><a href="index.html">Home</a></li>\
+  <li><a href="documentation/index.html">Documentation</a></li>\
+  <li><a href="https://github.com/michaelrsweet/Mini-XML/downloads">Download</a></li>\
+  <li><a href="https://github.com/michaelrsweet/Mini-XML/issues">Issues</a></li>\
+  <li><a href="https://github.com/michaelrsweet/Mini-XML/wiki">Wiki</a></li>\
+</ul>';
+
+  if (document.anchors.length > 0)
+  {
+    contents = contents + '\n<p>This Page:</p>\n<ul>\n';
+    for (i = 0; i < document.anchors.length; i ++)
+      contents = contents + '  <li><a href="#' + document.anchors[i].name +
+                 '">' + document.anchors[i].innerHTML + '</a></li>\n';
+    contents = contents + '</ul>';
+  }
+
+  document.getElementById('sidebar').innerHTML = contents;
+}
